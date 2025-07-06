a782bc4664fbfe2e82bda916a1ca7592a61e734e
rythcal-xyz <royethcalcaben17@gmail.com>
2025-07-06 11:31:00 +0800

    What is JavaScript?

---
+++ javascript-essentials/00-what-is-javascript/what-is-javascript.js
@@ -0,0 +1,54 @@
+//What is JavaScript?
+
+// JavaScript is a high-level, interpreted programming language used to create dynamic and interactive content on web pages.
+// JavaScript is a powerful, lightweight, high-level scripting language used to make web pages interactive and dynamic.
+// It’s one of the three core technologies of the web, along with HTML and CSS.
+// If HTML is the structure (like bones) and CSS is the style (like clothes), then JavaScript is the brain – making things respond, react, and move.
+
+// In simpler terms:
+//      1. It makes web pages do things instead of just sitting there.
+//      2. It is the third core technology of the Web:
+
+//      HTML: structure/content
+//      CSS: style/layout
+//      JavaScript: behavior/interactivity
+
+
+//  2. Server-side JavaScript
+//      Runs on a server with platforms like Node.js.
+//      Example: Powering APIs or databases.
+
+
+// JavaScript in the Real World
+//      You’ve seen JavaScript in action every day, even if you didn’t know it:
+//      When you click a button and something happens without reloading the page – that's JavaScript.
+//      When a website asks you to confirm your email is valid – that’s JavaScript.
+//      When a slideshow or carousel automatically moves – that’s JavaScript.
+//      When you type in a search box and see suggestions appear – again, JavaScript.
+
+
+// How It Works
+//      JavaScript runs directly inside your web browser.
+//      All modern browsers like Chrome, Firefox, Safari, and Edge have a built-in JavaScript engine.
+//      For example:
+//          Chrome uses V8
+//          Firefox uses SpiderMonkey
+
+
+// Where JavaScript run?
+
+//  1. Client-side JavaScript
+//      Runs in your browser.
+//      Most common use case.
+//      Example: Making a menu slide open.
+
+
+// Example Code:
+// <button onclick="sayHello()">Click Me</button>
+
+// <script>
+//  function sayHello() {
+//    alert("Hello, World!");
+//  }
+// </script>
+
